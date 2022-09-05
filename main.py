678a9187f89ddaf34df10436f324ad20ffc08e54
André Barreira Dinis <107752110+andrebdinis@users.noreply.github.com>
2022-09-05 20:12:35 +0000

    Hat class and experiment  function implemented.

--- main.py
+++ main.py
@@ -2,15 +2,23 @@
 import prob_calculator
 from unittest import main
 
+# PERSONAL NOTE: random.seed(95) is defined with a constant number so that freeCodeCamp can run their tests on a same generated sequence (if you pass a constant number as a random.seed() argument, the generated sequence will always be the same).
 prob_calculator.random.seed(95)
+
+
+# Example test
 hat = prob_calculator.Hat(blue=4, red=2, green=6)
-probability = prob_calculator.experiment(
-    hat=hat,
-    expected_balls={"blue": 2,
-                    "red": 1},
-    num_balls_drawn=4,
-    num_experiments=3000)
-print("Probability:", probability)
+expected_balls = {"blue": 2, "red": 1}
+num_balls_drawn = 4
+num_experiments = 3000
+probability = prob_calculator.experiment(hat=hat, expected_balls=expected_balls, num_balls_drawn=num_balls_drawn, num_experiments=num_experiments)
+print("Hat Contents:\n", hat.contents)
+print("Expected Balls:", expected_balls)
+print("Number of Balls Drawn:", num_balls_drawn)
+print("Number of Experiments:", num_experiments)
+formattedProbability = str(round(probability * 100, 1))
+print("Probability: " + formattedProbability + "%")
+
 
 # Run unit tests automatically
 main(module='test_module', exit=False)
--- prob_calculator.py
+++ prob_calculator.py
@@ -2,7 +2,36 @@ import copy
 import random
 # Consider using the modules imported above.
 
+
+# OBJECTIVE: Write a program to determine the approximate probability of drawing certain balls randomly from a hat.
+
+
 class Hat:
+  contents = list()
+
+  def __init__(self, **kwargs):
+    self.contents = []
+    for color,numBalls in kwargs.items():
+      for ball in range(numBalls):
+        self.contents.append(color)
+
+  # draw a number of balls from the hat
+  def draw(self, nBallsToDraw):
+    if nBallsToDraw > len(self.contents): return self.contents
+    sample = random.sample(self.contents, k=nBallsToDraw)
+    for drawnBall in sample:
+      self.contents.remove(drawnBall)
+    return sample
 
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
+  count = 0
+  for experiment in range(num_experiments):
+    hatObj = copy.deepcopy(hat)
+    match = False
+    draw = hatObj.draw(num_balls_drawn)
+    for color,numBalls in expected_balls.items():
+      if draw.count(color) >= numBalls: match = True; continue
+      match = False; break
+    if match: count += 1;
+  return (count / num_experiments)
